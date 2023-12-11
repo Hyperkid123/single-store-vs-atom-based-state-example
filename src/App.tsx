758a8a4defb465e47693f75ef4642e132cd5d63f
Hyperkid123 <marvusm.mmi@gmail.com>
2023-12-11 14:53:12 +0100

    Wrap layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
+import { PropsWithChildren } from 'react';
 import JotaiExample from './jotai/JotaiExample';
 import OptimizedZustandExample from './optimizedZustand/OptimizedZustandExample';
 import ReduxExample from './redux/ReduxExample';
 import ZustandExample from './zustand/ZustandExample';
 
+const Wrapper = ({ children }: PropsWithChildren) => {
+  return <div style={{ margin: 16 }}>{children}</div>;
+};
+
 function App() {
   return (
-    <div style={{ display: 'flex', padding: '1rem' }}>
-      <JotaiExample />
-      <hr />
-      <ReduxExample />
-      <hr />
-      <ZustandExample />
-      <hr />
-      <OptimizedZustandExample />
+    <div style={{ display: 'flex', padding: '1rem', flexWrap: 'wrap' }}>
+      <Wrapper>
+        <JotaiExample />
+      </Wrapper>
+      <Wrapper>
+        <ReduxExample />
+      </Wrapper>
+      <Wrapper>
+        <ZustandExample />
+      </Wrapper>
+      <Wrapper>
+        <OptimizedZustandExample />
+      </Wrapper>
     </div>
   );
 }
